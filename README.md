7c1d8a292509748235decc9483e39f12b40d6c67
Selbosh <david@selbys.org.uk>
2017-05-11 20:11:48 +0100

    Fix shell script: remove repeated Rscript and use correct #!/bin/sh header

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# travisbook
+# Testing bookdown with Travis CI
+
+[![Build Status](https://travis-ci.org/Selbosh/travisbook.svg?branch=master)](https://travis-ci.org/Selbosh/travisbook)
---
+++ _deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Clones the GitHub repository to the /docs/ directory.
+git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git docs
+cd docs
+
+# Clears out anything already in /docs/
+git rm -rf *
+
+# Copies compiled output from the /_book/ folder to /docs/
+# (_book is the default 'output_dir' in _bookdown.yml)
+cp -r ../_book/* ./
+
+# Commits + pushes everything in /docs/ to GitHub
+git add --all *
+git commit -m "Update the book"
+git push -q origin master
